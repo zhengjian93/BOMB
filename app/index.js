33d550c415d00d6287c087237855ba2dca4eebd6
zhengjian93 <zhengjian1993@163.com>
2018-06-29 16:28:18 +0800

    示例图片

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const compression = require('compression');
 const logger = require('./Logger.js');
+var test = require('./service/query.js');
 
 app.use(compression());
 app.use(express.static(__dirname+ '/game'));
@@ -9,6 +10,10 @@ app.use(express.static(__dirname+ '/game'));
 app.get('/',
   function(req, res) {
     res.sendFile(__dirname + '/game/game.html', { user: req.user });
+});
+
+app.get('/test',function(req,res){
+  test.geTest(req,res);
 });
 
 var server = app.listen(8081, function () {
